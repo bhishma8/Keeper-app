2efdce9c1059f59675778538da2c4ef42a85fa81
Ishan Sarma <ishan.baban@gmail.com>
2020-08-23 15:58:57 +0530

    add props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes";
+
+function createNote(text) {
+  return <Note />;
+}
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      <div>{notes.map(createNote)}</div>
       <Footer />
     </div>
   );
